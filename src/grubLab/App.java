7fab11a3356e125e6367f3f402a46aaa15bdce07
SK207_39 <SK207_39@K207_39.carrollcc.edu>
2016-11-28 18:08:56 -0500

    updated 11/28 6:08PM

--- src/grubLab/App.java
+++ src/grubLab/App.java
@@ -1,10 +1,33 @@
 package grubLab;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class App {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws SQLException{
+	    {
+	    	final String DB_URL = "jdbc:hsqldb:file:GrubDB/grub";
+	    	
+	    	
+	    	String sqlStatement = "SELECT * FROM GRUB ORDER BY DISH";
+	    	
+	    	Connection conn = DriverManager.getConnection(DB_URL);
+	    	
+	    	Statement stmt = conn.createStatement();
+	    	
+	    	ResultSet result = stmt.executeQuery(sqlStatement);
+	    	
+	    	while(result.next()){
+	    		System.out.println(result.getString("dish") + " " + result.getDouble("price"));
+	    	}
+	    	
+	    	conn.close();
+
+	    }
 
 	}
-
 }
